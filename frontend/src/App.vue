14bd2301796696b4b6bb169ebb29871f7b39219b
nand <ndeschildre@gmail.com>
2024-11-28 12:52:06 +0100

    Frontend example: Basic vue+vite

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,30 @@
 <script setup>
-import { Suspense } from 'vue';
-import LeftBar from './components/LeftBar.vue';
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <Suspense>
-    <div class="root">
-      <LeftBar />
-      <RouterView />
-    </div>
-  </Suspense>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-  .root {
-    display: flex;
-    gap: 80px;
-  }
-
-  @media (max-width: 768px) {
-    .root {
-      flex-direction: column;
-      gap: 0;
-    }
-  }
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
