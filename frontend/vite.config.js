e63634a6efd8e8778091925ef27e69721f5d2c21
nand <ndeschildre@gmail.com>
2024-11-29 10:05:34 +0100

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Starter Kit Plugin: Added this setting so that the app can be served from any subdirectory
+  base: "./",
+  build: {
+    target: "esnext"
+  },
 })
